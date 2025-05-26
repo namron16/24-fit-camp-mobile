e260894682b620581f25f2fc87d80463d3ff4d1c
namron16 <namronpontillo@gmail.com>
2025-05-26 08:04:24 +0800

    created .env file and refactor fetch url

--- src/utils/FetchData.js
+++ src/utils/FetchData.js
@@ -6,13 +6,109 @@ import {
 } from "@tanstack/react-query";
 
 import axios from "axios";
+import API from "../services/api";
 
+// //fetch member my id
+// export const useFetchMembersDetail = (id) => {
+//   const { data } = useSuspenseQuery({
+//     queryKey: ["members", id],
+//     queryFn: async () => {
+//       return await axios.get(`http://localhost:4001/members/${id}`);
+//     },
+//     staleTime: 1000 * 60 * 5,
+//   });
+//   return { memberDetails: data };
+// };
+// //fetch single member
+// export const useFetchMember = () => {
+//   const { data } = useQuery({
+//     queryKey: ["members"],
+//     queryFn: async () => {
+//       return await axios.get("http://localhost:4001/members");
+//     },
+//     staleTime: 1000 * 60 * 5,
+//   });
+//   return { member: data };
+// };
+
+// //edit member profile
+// export const useEditMember = (id) => {
+//   const queryClient = useQueryClient();
+//   const { mutateAsync: editMember } = useMutation({
+//     mutationFn: (updatedMember) =>
+//       axios.put(`http://localhost:4001/members/${id}`, updatedMember, {
+//         headers: { "Content-Type": "application/json" },
+//       }),
+//     onMutate: async (updatedMember) => {
+//       await queryClient.cancelQueries(["members", id]);
+//       await queryClient.cancelQueries(["members"]);
+
+//       const prevMemberData = queryClient.getQueryData(["members", id]);
+//       const prevMembersData = queryClient.getQueryData(["members"]);
+
+//       if (prevMemberData) {
+//         queryClient.setQueryData(["members", id], {
+//           ...prevMemberData,
+//           ...updatedMember,
+//         });
+//       }
+
+//       if (prevMembersData) {
+//         if (prevMembersData.data && Array.isArray(prevMembersData.data)) {
+//           queryClient.setQueryData(["members"], {
+//             ...prevMembersData,
+//             data: prevMembersData.data.map((member) =>
+//               member.id === id ? { ...member, ...updatedMember } : member
+//             ),
+//           });
+//         } else if (Array.isArray(prevMembersData)) {
+//           queryClient.setQueryData(
+//             ["members"],
+//             prevMembersData.map((member) =>
+//               member.id === id ? { ...member, ...updatedMember } : member
+//             )
+//           );
+//         }
+//       }
+
+//       return { prevMemberData, prevMembersData };
+//     },
+
+//     onError: (_error, _vars, context) => {
+//       if (context?.prevMemberData) {
+//         queryClient.setQueryData(["members", id], context.prevMemberData);
+//       }
+//       if (context?.prevMembersData) {
+//         queryClient.setQueryData(["members"], context.prevMembersData);
+//       }
+//     },
+//     onSettled: () => {
+//       queryClient.invalidateQueries(["members", id]);
+//       queryClient.invalidateQueries(["members"]);
+//     },
+//   });
+
+//   return { editMember };
+// };
+
+// //rewards hook
+// export const useFetchRewards = () => {
+//   const { data } = useSuspenseQuery({
+//     queryKey: ["rewards"],
+//     queryFn: async () => {
+//       return await axios.get("http://localhost:4001/rewards");
+//     },
+//     staleTime: 1000 * 60 * 5,
+//   });
+
+//   return { rewards: data };
+// };
 //fetch member my id
 export const useFetchMembersDetail = (id) => {
   const { data } = useSuspenseQuery({
     queryKey: ["members", id],
     queryFn: async () => {
-      return await axios.get(`http://localhost:4001/members/${id}`);
+      return await API.get(`/members/${id}`);
     },
     staleTime: 1000 * 60 * 5,
   });
@@ -23,7 +119,7 @@ export const useFetchMember = () => {
   const { data } = useQuery({
     queryKey: ["members"],
     queryFn: async () => {
-      return await axios.get("http://localhost:4001/members");
+      return await API.get("/members");
     },
     staleTime: 1000 * 60 * 5,
   });
@@ -35,7 +131,7 @@ export const useEditMember = (id) => {
   const queryClient = useQueryClient();
   const { mutateAsync: editMember } = useMutation({
     mutationFn: (updatedMember) =>
-      axios.put(`http://localhost:4001/members/${id}`, updatedMember, {
+      API.put(`/members/${id}`, updatedMember, {
         headers: { "Content-Type": "application/json" },
       }),
     onMutate: async (updatedMember) => {
@@ -95,7 +191,7 @@ export const useFetchRewards = () => {
   const { data } = useSuspenseQuery({
     queryKey: ["rewards"],
     queryFn: async () => {
-      return await axios.get("http://localhost:4001/rewards");
+      return await API.get("/rewards");
     },
     staleTime: 1000 * 60 * 5,
   });
